efab349a9726748667569ed666900d7325204458
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-06-13 12:24:04 +0530

    course details accordion

---
+++ codekrafters/src/components/CourseAccordion.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const CourseAccordion = () => {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography sx={{ fontSize: '25px', fontFamily: 'Roboto', fontWeight: 'bold' }}>
+            Unit 0 - Foundation Of Programming
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography sx={{ fontSize: '25px',  fontFamily: 'Roboto', fontWeight: 'bold' }}>
+            Unit 1 - Data Structures and Algorithms
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography sx={{ fontSize: '25px', fontFamily: 'Roboto', fontWeight: 'bold' }}>
+            Unit 2 - System Design in Depth
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography sx={{ fontSize: '25px', fontFamily: 'Roboto', fontWeight: 'bold' }}>
+            Unit 3 - Advanced Data Structures & Algorithms
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography sx={{ fontSize: '25px', fontFamily: 'Roboto', fontWeight: 'bold' }}>
+            Unit 4 - Electives & Specialization
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography sx={{ fontSize: '25px', fontFamily: 'Roboto', fontWeight: 'bold' }}>
+            Unit 5 - Work on End to End Projects in Partnership with Startups
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
+
+export default CourseAccordion;
--- codekrafters/src/components/CourseDemo.jsx
+++ codekrafters/src/components/CourseDemo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../styles/CourseDemo.css"
+import CourseAccordion from './CourseAccordion'
 const CourseDemo = () => {
   return (
     <div className='course-demo-main'>
@@ -8,7 +9,9 @@ const CourseDemo = () => {
         <div className="details">Our Comprehensive curriculum, Designed for professionals</div>
       </div>
       <div className="course-section">
-        
+        <div className="course-accordion">
+          <CourseAccordion/>
+        </div>
       </div>
     </div>
   )
